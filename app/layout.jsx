3348b7aa8b18f041dc1948d2a3a4e6974f470b4f
aktnb <70847394+aktnb@users.noreply.github.com>
2025-10-03 19:54:04 +0900

    Update layout component with feedback and navigation props; localize welcome message in index

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,8 +37,17 @@ export default async function RootLayout({ children }) {
           // banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
           footer={footer}
+          feedback={{
+            content: null,
+            labels: "",
+            link: ""
+          }}
+          navigation={{
+            prev: false,
+            next: false
+          }}
+          editLink={null}
         // ... Your additional layout options
         >
           {children}
