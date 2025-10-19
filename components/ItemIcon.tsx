1ec2a0619d6f6dd080e32e40d0a99deee8baf92f
aktnb <70847394+aktnb@users.noreply.github.com>
2025-10-19 18:13:10 +0900

    Add item icons and global styles for witch trap documentation

---
+++ components/ItemIcon.tsx
@@ -0,0 +1,5 @@
+import Image from 'next/image'
+
+export function ItemIcon({ item, size = 24 }: { item: string, size: number }) {
+  return <Image src={`/textures/${item}.png`} className="item-icon" width={size} height={size} alt={item} />;
+}
